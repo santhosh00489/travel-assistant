387444e2ca8dbdb9007754575a3ff8ae5d8dd006
santhosh00489 <155361311+santhosh00489@users.noreply.github.com>
2024-01-17 19:17:16 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("CarTalk AI: Unleashing Automotive Wisdom")
+st.title("ChidambaramLocator")
 
 question = st.text_input("Question: ")
 
