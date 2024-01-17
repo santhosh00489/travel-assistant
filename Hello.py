3d4881906a1663706c736b734d7738b749376fda
santhosh00489 <155361311+santhosh00489@users.noreply.github.com>
2024-01-17 19:27:54 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ from langchain_helper import get_few_shot_db_chain
 st.title("CDM Info Hub")
 
 question = st.text_input("QUESTION: ")
-
+if st.button("Enter"):
 if question:
     chain = get_few_shot_db_chain()
     response = chain.run(question)
