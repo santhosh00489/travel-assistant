6016854a7ddf1c62a3a7dcbaff897a11a7ed6760
santhosh00489 <155361311+santhosh00489@users.noreply.github.com>
2024-01-17 19:28:53 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
 st.title("CDM Info Hub")
@@ -10,4 +10,18 @@ if question:
     response = chain.run(question)
 
     st.header("ANSWER")
-    st.write(response)
+    st.write(response)'''
+import streamlit as st
+from langchain_helper import get_few_shot_db_chain
+
+st.title("CDM Info Hub")
+
+question = st.text_input("QUESTION: ")
+
+if st.button("Enter"):
+    if question:
+        chain = get_few_shot_db_chain()
+        response = chain.run(question)
+
+        st.header("ANSWER")
+        st.write(response)
